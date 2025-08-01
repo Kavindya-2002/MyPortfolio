398fe8815610b8fbebb82e70164f10b0b0212443
Kavindya Bandara <kavindya456789@gmail.com>
2025-08-01 18:40:20 +0530

    Add hamburger feature to the nav bar and meke this porfolio responsive.

--- main.js
+++ main.js
@@ -6,6 +6,14 @@ var typed= new Typed(".text", {
     loop: true
 });
 
+const hamburger = document.getElementById("hamburger");
+const navbar = document.getElementById("navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("open");
+});
+
+
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault(); 
   
